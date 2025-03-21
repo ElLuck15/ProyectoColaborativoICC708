4bb14227d95ff78e66e300439d9d78e6615aa845
ElLuck15 <sebitaluck2015@gmail.com>
2025-03-21 12:19:42 -0300

    DEVELOP 3

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
     public static List<Float> notas = new ArrayList<>();
     public static void main(String[] args) {
         IngresarNotas();
     }
-    public static void IngresarNotas(){
+    public static void IngresarNotas() {
         Scanner scanner = new Scanner(System.in);
         Float nota;
-        do{
+        do {
             System.out.println("Ingrese una nota o si no quiere agregar notas ingrese -1");
             nota = scanner.nextFloat();
-            if(nota!=-1){
+            if (nota != -1) {
                 notas.add(nota);
             }
-        }while(nota != -1);
+        } while (nota != -1);
         System.out.println(nota);
+    }
 
     public static void MostrarResultado(Float avg){
         System.out.println("El promedio es "+avg);
