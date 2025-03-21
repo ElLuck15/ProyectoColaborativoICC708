a0e04f431d514f3d917298a5f645fefc9e137a4d
ElLuck15 <sebitaluck2015@gmail.com>
2025-03-21 11:56:15 -0300

    Processing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Siiii!");
+
+    }
+    public static Float PromedioNotas(List<Float> notas){
+        float avg = 0;
+        for (int i = 0;i<notas.size();i++){
+            avg+= notas.get(i);
+        }
+        return avg/notas.size();
     }
 }
 //Hola
