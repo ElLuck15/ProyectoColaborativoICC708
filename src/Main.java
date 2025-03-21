359245b05287d5b9ed40ce49b70726aae14774d5
ElLuck15 <sebitaluck2015@gmail.com>
2025-03-21 11:58:06 -0300

    Output

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Siiii!");
     }
+    public static void MostrarResultado(Float avg){
+        System.out.println("El promedio es "+avg);
+    }
 }
 //Hola
 //Que onda cracks
